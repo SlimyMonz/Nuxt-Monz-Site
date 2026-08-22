46c205d592f13c4bfb46ecb8bc2de69a5b73929d
Monz <monz@noreply.codeberg.org>
2026-08-22 02:34:52 +0200

    Added manifest and baseUrl

---
+++ app/data/art.ts
@@ -0,0 +1,196 @@
+export const baseUrl = "https://mega.nz/folder/ha13FDAI#LeQHlDBWrwbz6rZLH8InxQ"
+
+interface ManifestEntry {
+  id: string;
+  file: string;
+}
+
+export const manifest: Record<string, ManifestEntry[]> = {
+  "2009": [
+    { id: "art_2009_1", file: "art_2009_1.jpg" },
+    { id: "art_2009_2", file: "art_2009_2.jpg" },
+    { id: "art_2009_3", file: "art_2009_3.jpg" },
+    { id: "art_2009_4", file: "art_2009_4.jpg" },
+    { id: "art_2009_5", file: "art_2009_5.jpg" },
+    { id: "art_2009_6", file: "art_2009_6.jpg" },
+  ],
+
+  "2010": [
+    { id: "art_2010_1", file: "art_2010_1.jpg" },
+    { id: "art_2010_2", file: "art_2010_2.jpg" },
+    { id: "art_2010_3", file: "art_2010_3.jpg" },
+    { id: "art_2010_4", file: "art_2010_4.jpg" },
+    { id: "art_2010_5", file: "art_2010_5.jpg" },
+    { id: "art_2010_6", file: "art_2010_6.jpg" },
+  ],
+
+  "2011": [
+    { id: "art_2011_1", file: "art_2011_1.jpg" },
+    { id: "art_2011_2", file: "art_2011_2.jpg" },
+    { id: "art_2011_3", file: "art_2011_3.jpg" },
+    { id: "art_2011_4", file: "art_2011_4.jpg" },
+    { id: "art_2011_5", file: "art_2011_5.jpg" },
+  ],
+
+  "2012": [
+    { id: "art_2012_1", file: "art_2012_1.jpg" },
+    { id: "art_2012_2", file: "art_2012_2.jpg" },
+    { id: "art_2012_3", file: "art_2012_3.jpg" },
+    { id: "art_2012_4", file: "art_2012_4.jpg" },
+    { id: "art_2012_5", file: "art_2012_5.jpg" },
+    { id: "art_2012_6", file: "art_2012_6.jpg" },
+    { id: "art_2012_7", file: "art_2012_7.jpg" },
+    { id: "art_2012_8", file: "art_2012_8.jpg" },
+  ],
+
+  "2013": [
+    { id: "art_2013_1", file: "art_2013_1.jpg" },
+    { id: "art_2013_2", file: "art_2013_2.jpg" },
+    { id: "art_2013_3", file: "art_2013_3.jpg" },
+  ],
+
+  "2014": [
+    { id: "art_2014_1", file: "art_2014_1.jpg" },
+  ],
+
+  "2015": [
+    { id: "art_2015_1", file: "art_2015_1.jpg" },
+    { id: "art_2015_2", file: "art_2015_2.jpg" },
+    { id: "art_2015_3", file: "art_2015_3.jpg" },
+    { id: "art_2015_4", file: "art_2015_4.jpg" },
+    { id: "art_2015_5", file: "art_2015_5.jpg" },
+    { id: "art_2015_6", file: "art_2015_6.jpg" },
+  ],
+
+  "2016": [
+    { id: "art_2016_1", file: "art_2016_1.jpg" },
+    { id: "art_2016_10", file: "art_2016_10.jpg" },
+    { id: "art_2016_11", file: "art_2016_11.jpg" },
+    { id: "art_2016_12", file: "art_2016_12.jpg" },
+    { id: "art_2016_13", file: "art_2016_13.jpg" },
+    { id: "art_2016_2", file: "art_2016_2.jpg" },
+    { id: "art_2016_3", file: "art_2016_3.jpg" },
+    { id: "art_2016_4", file: "art_2016_4.jpg" },
+    { id: "art_2016_5", file: "art_2016_5.jpg" },
+    { id: "art_2016_6", file: "art_2016_6.jpg" },
+    { id: "art_2016_7", file: "art_2016_7.jpg" },
+    { id: "art_2016_8", file: "art_2016_8.jpg" },
+    { id: "art_2016_9", file: "art_2016_9.jpg" },
+  ],
+
+  "2017": [
+    { id: "art_2017_1", file: "art_2017_1.jpg" },
+    { id: "art_2017_10", file: "art_2017_10.jpg" },
+    { id: "art_2017_11", file: "art_2017_11.jpg" },
+    { id: "art_2017_12", file: "art_2017_12.jpg" },
+    { id: "art_2017_2", file: "art_2017_2.jpg" },
+    { id: "art_2017_3", file: "art_2017_3.jpg" },
+    { id: "art_2017_4", file: "art_2017_4.jpg" },
+    { id: "art_2017_5", file: "art_2017_5.jpg" },
+    { id: "art_2017_6", file: "art_2017_6.jpg" },
+    { id: "art_2017_7", file: "art_2017_7.jpg" },
+    { id: "art_2017_8", file: "art_2017_8.jpg" },
+    { id: "art_2017_9", file: "art_2017_9.jpg" },
+  ],
+
+  "2018": [
+    { id: "art_2018_1", file: "art_2018_1.jpg" },
+    { id: "art_2018_2", file: "art_2018_2.jpg" },
+    { id: "art_2018_3", file: "art_2018_3.jpg" },
+    { id: "art_2018_4", file: "art_2018_4.jpg" },
+    { id: "art_2018_5", file: "art_2018_5.jpg" },
+    { id: "art_2018_6", file: "art_2018_6.jpg" },
+    { id: "art_2018_7", file: "art_2018_7.jpg" },
+    { id: "art_2018_8", file: "art_2018_8.jpg" },
+    { id: "art_2018_9", file: "art_2018_9.jpg" },
+  ],
+
+  "2019": [
+    { id: "art_2019_1", file: "art_2019_1.jpg" },
+    { id: "art_2019_10", file: "art_2019_10.jpg" },
+    { id: "art_2019_11", file: "art_2019_11.jpg" },
+    { id: "art_2019_12", file: "art_2019_12.jpg" },
+    { id: "art_2019_13", file: "art_2019_13.jpg" },
+    { id: "art_2019_14", file: "art_2019_14.jpg" },
+    { id: "art_2019_15", file: "art_2019_15.jpg" },
+    { id: "art_2019_16", file: "art_2019_16.jpg" },
+    { id: "art_2019_2", file: "art_2019_2.jpg" },
+    { id: "art_2019_3", file: "art_2019_3.jpg" },
+    { id: "art_2019_4", file: "art_2019_4.jpg" },
+    { id: "art_2019_5", file: "art_2019_5.jpg" },
+    { id: "art_2019_6", file: "art_2019_6.jpg" },
+    { id: "art_2019_7", file: "art_2019_7.jpg" },
+    { id: "art_2019_8", file: "art_2019_8.jpg" },
+    { id: "art_2019_9", file: "art_2019_9.jpg" },
+  ],
+
+  "2020": [
+    { id: "art_2020_1", file: "art_2020_1.jpg" },
+    { id: "art_2020_2", file: "art_2020_2.jpg" },
+    { id: "art_2020_3", file: "art_2020_3.jpg" },
+  ],
+
+  "2021": [
+    { id: "art_2021_1", file: "art_2021_1.jpg" },
+    { id: "art_2021_10", file: "art_2021_10.jpg" },
+    { id: "art_2021_11", file: "art_2021_11.jpg" },
+    { id: "art_2021_12", file: "art_2021_12.jpg" },
+    { id: "art_2021_13", file: "art_2021_13.jpg" },
+    { id: "art_2021_14", file: "art_2021_14.jpg" },
+    { id: "art_2021_15", file: "art_2021_15.jpg" },
+    { id: "art_2021_16", file: "art_2021_16.jpg" },
+    { id: "art_2021_17", file: "art_2021_17.jpg" },
+    { id: "art_2021_18", file: "art_2021_18.jpg" },
+    { id: "art_2021_19", file: "art_2021_19.jpg" },
+    { id: "art_2021_2", file: "art_2021_2.jpg" },
+    { id: "art_2021_20", file: "art_2021_20.jpg" },
+    { id: "art_2021_21", file: "art_2021_21.jpg" },
+    { id: "art_2021_22", file: "art_2021_22.jpg" },
+    { id: "art_2021_23", file: "art_2021_23.jpg" },
+    { id: "art_2021_24", file: "art_2021_24.jpg" },
+    { id: "art_2021_25", file: "art_2021_25.jpg" },
+    { id: "art_2021_26", file: "art_2021_26.jpg" },
+    { id: "art_2021_27", file: "art_2021_27.jpg" },
+    { id: "art_2021_28", file: "art_2021_28.jpg" },
+    { id: "art_2021_3", file: "art_2021_3.jpg" },
+    { id: "art_2021_4", file: "art_2021_4.jpg" },
+    { id: "art_2021_5", file: "art_2021_5.jpg" },
+    { id: "art_2021_6", file: "art_2021_6.jpg" },
+    { id: "art_2021_7", file: "art_2021_7.jpg" },
+    { id: "art_2021_8", file: "art_2021_8.jpg" },
+    { id: "art_2021_9", file: "art_2021_9.jpg" },
+  ],
+
+  "2022": [
+    { id: "art_2022_1", file: "art_2022_1.jpg" },
+    { id: "art_2022_2", file: "art_2022_2.jpg" },
+    { id: "art_2022_3", file: "art_2022_3.jpg" },
+    { id: "art_2022_4", file: "art_2022_4.jpg" },
+    { id: "art_2022_5", file: "art_2022_5.jpg" },
+    { id: "art_2022_6", file: "art_2022_6.jpg" },
+  ],
+
+  "2023": [
+    { id: "art_2023_1", file: "art_2023_1.jpg" },
+    { id: "art_2023_2", file: "art_2023_2.jpg" },
+    { id: "art_2023_3", file: "art_2023_3.jpg" },
+    { id: "art_2023_4", file: "art_2023_4.jpg" },
+    { id: "art_2023_5", file: "art_2023_5.jpg" },
+    { id: "art_2023_6", file: "art_2023_6.jpg" },
+  ],
+
+  "2025": [
+    { id: "art_2025_1", file: "art_2025_1.svg" },
+    { id: "art_2025_10", file: "art_2025_10.svg" },
+    { id: "art_2025_11", file: "art_2025_11.svg" },
+    { id: "art_2025_12", file: "art_2025_12.svg" },
+    { id: "art_2025_2", file: "art_2025_2.jpg" },
+    { id: "art_2025_3", file: "art_2025_3.svg" },
+    { id: "art_2025_4", file: "art_2025_4.svg" },
+    { id: "art_2025_5", file: "art_2025_5.svg" },
+    { id: "art_2025_6", file: "art_2025_6.svg" },
+    { id: "art_2025_7", file: "art_2025_7.jpg" },
+    { id: "art_2025_8", file: "art_2025_8.jpg" },
+    { id: "art_2025_9", file: "art_2025_9.jpg" },
+  ],
+};
