6c4db52924929cb0fb8fe28f9c458d5528c02b5b
Monz <monz@noreply.codeberg.org>
2026-08-22 02:42:13 +0200

    Added the proper baseUrl

--- app/data/art.ts
+++ app/data/art.ts
@@ -1,4 +1,4 @@
-export const baseUrl = "https://mega.nz/folder/ha13FDAI#LeQHlDBWrwbz6rZLH8InxQ"
+export const baseUrl = "https://s3.g.megas4.com/reda2vxvgd2tcjeziyx5yjfoizlcthi5dza4x/Art";
 
 interface ManifestEntry {
   id: string;
