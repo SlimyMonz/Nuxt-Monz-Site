fe1a8c4a1f2eabf9e041b3acdd63f29307a7cb05
Slimy Monster <code@monz.us>
2026-08-23 20:23:25 -0400

    Added more albums to fill out the page.

--- app/data/music.ts
+++ app/data/music.ts
@@ -1,43 +1,107 @@
 interface Album {
-  title: string
-  artist: string
-  year: string
-  img: string
-  url: string
-  body: string
+  title: string;
+  artist: string;
+  year: string;
+  img: string;
+  url: string;
+  body: string;
 }
 
 export const albums: Album[] = [
   {
-    title: 'Mayhem',
-    artist: 'Lady Gaga',
-    year: '2025',
-    img: 'https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/85/96/e5/8596e5c9-ea5d-f074-14ab-0def74d49a41/Job55a60f4d-834e-43b4-b0ac-bc65708cbd78-187464330-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_366977506_2078765148-Time1741332248310.png/592x592bb.webp',
-    url: 'https://music.apple.com/us/album/mayhem/1792666546',
-    body: 'Abracadabra is the standout here, but the whole album is electrifyingly good. This is the best Lady Gaga has ever been.'
+    title: "Language & Perspective",
+    artist: "Bad Suns",
+    year: "2014",
+    img: "https://f4.bcbits.com/img/a1253420473_1x1_700.avif",
+    url: "https://badsunsmusic.bandcamp.com/album/language-perspective",
+    body: 'Every song in this album fits a theme, but never feels boring. "Salt" and "Rearview" were easily my favorites here.',
   },
   {
-    title: 'Utopia',
-    artist: 'St. Lucia',
-    year: '2022',
-    img: 'https://f4.bcbits.com/img/a3964903351_10.jpg',
-    url: 'https://stluciamusic.bandcamp.com/album/utopia',
-    body: 'Tropic pop taken to the next level. A mix of upbeat and smooth, with vocals and instruments that carry you away.'
+    title: "Texture",
+    artist: "Battle Tapes",
+    year: "2023",
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Music221/v4/b0/31/80/b031809d-5df1-6998-6ea6-36aa05785572/617513547528.png/592x592bb.webp",
+    url: "https://music.apple.com/us/album/texture/1764653077",
+    body: 'Electrifying alternative rock that makes you grit your teeth and get your freak on. "One Night in Burbank" is heavy yet smooth.',
   },
   {
-    title: 'Bliss',
-    artist: 'Temples',
-    year: '2026',
-    img: 'https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/e3/56/77/e35677cf-5fb6-5d06-11d8-63733431a0e4/8717931356282.jpg/592x592bb.webp',
-    url: 'https://music.apple.com/us/album/bliss/1877638926',
-    body: 'Taking the sounds of the 1950s and adding an insanely modern energy with some fantastic production quality.'
+    title: "Brothertiger",
+    artist: "Brothertiger",
+    year: "2022",
+    img: "https://f4.bcbits.com/img/a2499038085_1x1_700.avif",
+    url: "https://brothertiger.bandcamp.com/album/brothertiger",
+    body: "Low-key synth pop and gentle vocals transports you to a world where the rainforest is alive and loves you. If a sunset could be bottled, here it is!",
   },
   {
-    title: 'As I Try Not To Fall Apart',
-    artist: 'White Lies',
-    year: '2022',
-    img: 'https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/ac/8a/d5/ac8ad52c-f66a-e842-ec57-c653d0f799c0/5400863100384_cover.jpg/592x592bb.webp',
-    url: 'https://music.apple.com/us/album/as-i-try-not-to-fall-apart-bonus-edition/1650376145',
-    body: 'The epitome of "sad white boy music" with a lot of upbeat instrumentals. Solid male vocalist and heart-breaking lyrics.'
-  }
-]
+    title: "Infinity On High",
+    artist: "Fall Out Boy",
+    year: "2007",
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/1e/02/58/1e0258e0-d921-b7d1-7519-74f31fed4391/07UMGIM03661.rgb.jpg/592x592bf.webp",
+    url: "https://music.apple.com/us/album/infinity-on-high/1440786414",
+    body: "If you are not already aware of this album: It is a must-listen. This is the zenith of alternative-pop-punk! Every single song rocks.",
+  },
+  {
+    title: "Koda Vista",
+    artist: "Joywave",
+    year: "2012",
+    img: "https://f4.bcbits.com/img/a2893020354_1x1_700.avif",
+    url: "https://joywave.bandcamp.com/album/koda-vista",
+    body: "Something deeply raw about this album compared to their other works. This alternative sound holds up to the test of time and beyond.",
+  },
+  {
+    title: "MAYHEM",
+    artist: "Lady Gaga",
+    year: "2025",
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Video221/v4/85/96/e5/8596e5c9-ea5d-f074-14ab-0def74d49a41/Job55a60f4d-834e-43b4-b0ac-bc65708cbd78-187464330-PreviewImage_Preview_Image_Intermediate_nonvideo_sdr_366977506_2078765148-Time1741332248310.png/592x592bb.webp",
+    url: "https://music.apple.com/us/album/mayhem/1792666546",
+    body: "Abracadabra is the standout here, but the whole album is electrifyingly good. This is the best Lady Gaga has ever been.",
+  },
+  {
+    title: "Wellnxss",
+    artist: "Last Dinosaurs",
+    year: "2026",
+    img: "https://f4.bcbits.com/img/a3605593939_1x1_700.avif",
+    url: "https://last-dinosaurs.bandcamp.com/album/wellnxss",
+    body: 'This 10th anniversary remake of an alternative album gives me a sense of nostalgia that I never had. One of the new songs, "Pink Flags", is a huge standout.',
+  },
+  {
+    title: "Utopia",
+    artist: "St. Lucia",
+    year: "2022",
+    img: "https://f4.bcbits.com/img/a3964903351_10.jpg",
+    url: "https://stluciamusic.bandcamp.com/album/utopia",
+    body: "Tropic pop taken to the next level. A mix of upbeat and smooth, with vocals and instruments that carry you away.",
+  },
+  {
+    title: "Surface Sound",
+    artist: "Sea Fever",
+    year: "2025",
+    img: "https://f4.bcbits.com/img/a3510693377_1x1_700.avif",
+    url: "https://seafeverband.bandcamp.com/album/surface-sound-2",
+    body: "The electronics hits paired with the layered vocals makes you want to get up and get shit done.",
+  },
+  {
+    title: "No Virtue",
+    artist: "Sights & Sounds",
+    year: "2019",
+    img: "https://f4.bcbits.com/img/a392328668_1x1_700.avif",
+    url: "https://sightsxsounds.bandcamp.com/album/no-virtue",
+    body: "A tragic alternative rock album that really cuts deep into the soul. Lots of outbursts and heavy hits with strong melodies.",
+  },
+  {
+    title: "Bliss",
+    artist: "Temples",
+    year: "2026",
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/e3/56/77/e35677cf-5fb6-5d06-11d8-63733431a0e4/8717931356282.jpg/592x592bb.webp",
+    url: "https://music.apple.com/us/album/bliss/1877638926",
+    body: "Taking the sounds of the 1950s and adding an insanely modern energy with some fantastic production quality.",
+  },
+  {
+    title: "As I Try Not To Fall Apart",
+    artist: "White Lies",
+    year: "2022",
+    img: "https://is1-ssl.mzstatic.com/image/thumb/Music112/v4/ac/8a/d5/ac8ad52c-f66a-e842-ec57-c653d0f799c0/5400863100384_cover.jpg/592x592bb.webp",
+    url: "https://music.apple.com/us/album/as-i-try-not-to-fall-apart-bonus-edition/1650376145",
+    body: 'The epitome of "sad white boy music" with a lot of upbeat instrumentals. Solid male vocalist and heart-breaking lyrics.',
+  },
+];
