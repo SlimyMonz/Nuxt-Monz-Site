227731d960b864752ef33b30c66d6c4b6fd04c62
Slimy Monster <code@monz.us>
2026-08-25 23:02:00 -0400

    Add character page.

---
+++ app/data/characters.ts
@@ -0,0 +1,38 @@
+interface character {
+  name: string;
+  img: string;
+  species: string;
+  description: string;
+  url: string;
+}
+
+export const characters: character[] = [
+  {
+    name: "Alexandrite",
+    img: "/characters/alexandrite.jpg",
+    species: "T-Rex, Dinosaur",
+    description: "Middle-aged, muscle-gut, t-rex dinosaur. A big bottom that loves it when dudes play with his ass.",
+    url: "https://www.furaffinity.net/gallery/slimymonz/folder/1198506/Alexandrite"
+  },
+  {
+    name: "Dan",
+    img: "/characters/dan.jpg",
+    species: "Alligator",
+    description: "Full name is Dorothy Heaver, but his gym bros call him Dan. He's totally not gay! Unless...",
+    url: "https://www.furaffinity.net/gallery/slimymonz/folder/1198512/Dan"
+  },
+  {
+    name: "Monz",
+    img: "/characters/monz.jpg",
+    species: "Demon, Slime Monster",
+    description: "This is my official 'self' as an online persona. I am Monz! Monz is me!",
+    url: "https://www.furaffinity.net/gallery/slimymonz/folder/1198298/Monz"
+  },
+  {
+    name: "Zenyth",
+    img: "/characters/zenyth.jpg",
+    species: "Demon",
+    description: "A holy man transformed into a big, gay, red demon. He loves Ignacis, his demon partner~",
+    url: "https://www.furaffinity.net/gallery/slimymonz/folder/1198328/Zenyth"
+  },
+]
--- app/pages/characters.vue
+++ app/pages/characters.vue
@@ -1,7 +1,53 @@
 <template>
     <UPageHero
-        headline="WORK IN PROGRESS"
+        headline="Yeah, they're all gay."
         title="Character Page"
-        description="All of my characters will go here with a nice lil summary and art."
+        description="Click on a character to view their FurAffinity gallery! Account is required and 18+ Only!"
     />
+    <UPageGrid>
+        <ULink
+            v-for="(character, i) in characters"
+            :key="i"
+            :to="character.url"
+            target="_blank"
+            class="block"
+        >
+            <UCard
+                class="h-full transition hover:ring-4 hover:ring-secondary"
+                :ui="{ body: 'p-0 sm:p-0' }"
+                :link="character.url"
+            >
+                <template #header>
+                    <div class="flex flex-col gap-2 text-center">
+                        <h2
+                            class="text-xl font-bold text-highlighted uppercase tracking-wide"
+                        >
+                            {{ character.name }}
+                        </h2>
+                    </div>
+                </template>
+                <img
+                    :src="character.img"
+                    :alt="`${character.img}`"
+                    class="h-full object-cover block"
+                />
+                <template #footer>
+                    <div class="flex flex-col gap-2 text-center">
+                        <p class="font-bold uppercase tracking-wide">
+                            {{ character.species }}
+                        </p>
+                        <p
+                            class="mt-2 w-full leading-relaxed border-t border-default pt-3 px-2 text-left"
+                        >
+                            {{ character.description }}
+                        </p>
+                    </div>
+                </template>
+            </UCard>
+        </ULink>
+    </UPageGrid>
 </template>
+
+<script setup lang="ts">
+import { characters } from "~/data/characters";
+</script>
