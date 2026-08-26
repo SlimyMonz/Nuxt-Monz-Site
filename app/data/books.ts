24addc690053bbea5491afe7cb2eb4a575d4c33a
Slimy Monster <code@monz.us>
2026-08-26 19:10:34 -0400

    Created books page.

---
+++ app/data/books.ts
@@ -0,0 +1,27 @@
+export interface Book {
+  title: string;
+  author: string;
+  date: string;
+  img: string;
+  description: string;
+  url: string;
+}
+
+export const books: Book[] = [
+  {
+    title: "The Three-Body Problem",
+    author: "Cixin Liu & Ken Liu",
+    date: "2014",
+    img: "/books/three_body_problem.jpg",
+    description: `A slow-starting science fiction story that really starts to take off once it gets going. Will need to be patient at first, but ends up being a wild ride.`,
+    url: "https://books.apple.com/us/book/the-three-body-problem/id856893409",
+  },
+  {
+    title: "Project Hail Mary",
+    author: "Andy Weir",
+    date: "2021",
+    img: "/books/project_hail_mary.jpg",
+    description: "Purely fun sci-fi with a touch of suspense and a lot of heart. Engaging without being sophisticated.",
+    url: "https://books.apple.com/us/book/project-hail-mary/id1526997052"
+  },
+];
--- app/pages/books.vue
+++ app/pages/books.vue
@@ -1,7 +1,59 @@
 <template>
-    <UPageHero
-        headline="WORK IN PROGRESS"
-        title="Books I've read"
-        description="When this page is made there will be a neat sort of bookshelf of stuff I've read and a mini-review."
-    />
+    <div class="mb-16">
+        <UPageHero
+            id="top"
+            headline="Do not eat!"
+            title="Monster Reads"
+            description="A collection of books I've read and enjoyed. Click one to buy on Apple Books!"
+        />
+
+        <UPageGrid>
+            <ULink
+                v-for="(book, i) in books"
+                :key="i"
+                :to="book.url"
+                target="_blank"
+                class="block"
+            >
+                <UCard
+                    class="h-full transition hover:ring-4 hover:ring-secondary"
+                    :ui="{ body: 'p-0 sm:p-0' }"
+                >
+                    <template #header>
+                        <div class="flex flex-col gap-0.5">
+                            <h2
+                                class="text-left text-lg font-bold text-highlighted truncate"
+                            >
+                                {{ book.title }}
+                            </h2>
+                            <div
+                                class="flex items-baseline justify-between gap-2"
+                            >
+                                <span class="text-left truncate">
+                                    {{ book.author }}
+                                </span>
+                                <span class="text-right">
+                                    {{ book.date }}
+                                </span>
+                            </div>
+                        </div>
+                    </template>
+                    <img
+                        :src="book.img"
+                        :alt="`${book.title} by ${book.author}`"
+                        class="w-full object-cover block"
+                    />
+                    <template #footer>
+                        <p>
+                            {{ book.description }}
+                        </p>
+                    </template>
+                </UCard>
+            </ULink>
+        </UPageGrid>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { books } from "~/data/books";
+</script>
