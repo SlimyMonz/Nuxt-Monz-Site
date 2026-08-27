52c1224289615a5f3bab082e39272ecbb253fdbf
Slimy Monster <code@monz.us>
2026-08-27 14:28:22 -0400

    More flowery text descriptions.

--- app/data/movies.ts
+++ app/data/movies.ts
@@ -13,7 +13,7 @@ export const Movies: Movie[] = [
     director: "John Carpenter",
     date: "1982",
     img: "/movies/the_thing.jpg",
-    description: `Horrifying terror. Crazy good practical effects. A must-watch for horror fans.`,
+    description: `Horrifying, tense, and packed with some of the best practical effects ever put to film. An absolute must-watch for horror fans.`,
     url: "https://tv.apple.com/us/movie/the-thing/umc.cmc.70lpgfopfk3gr1pg7uux69znx",
   },
   {
@@ -21,7 +21,7 @@ export const Movies: Movie[] = [
     director: "Christopher Smith",
     date: "2008",
     img: "/movies/triangle.jpg",
-    description: `Shocking and tragic. What is supposed to be a vacation away from the pressures of life ends up becoming something worse.`,
+    description: `Shocking and tragic. What begins as a vacation away from the pressures of everyday life slowly turns into something far more sinister.`,
     url: "https://tv.apple.com/us/movie/triangle/umc.cmc.5hcljk35jquf4c354pht2p8fw",
   },
   {
@@ -29,7 +29,7 @@ export const Movies: Movie[] = [
     director: "Dan Gilroy",
     date: "2014",
     img: "/movies/nightcrawler.jpg",
-    description: `Dramatic and crazy. Jake Gyllenhaal portrays an insanely dark persona who will do nothing to get what he wants.`,
+    description: `Dark, disturbing, and completely captivating. Jake Gyllenhaal delivers an incredible performance as a man willing to do almost anything to get what he wants.`,
     url: "https://tv.apple.com/us/movie/nightcrawler/umc.cmc.1h8fjzqshxpg22pa0bm7iho27",
   },
   {
@@ -37,7 +37,7 @@ export const Movies: Movie[] = [
     director: "Leigh Whannell",
     date: "2018",
     img: "/movies/upgrade.jpg",
-    description: `Unique lighting and fierce cinematography. Great sci-fi action movie that keeps the blood pumping.`,
+    description: `Stylish lighting, fierce cinematography, and plenty of brutal action. A fantastic sci-fi thriller that never lets the adrenaline drop.`,
     url: "https://tv.apple.com/us/movie/upgrade/umc.cmc.5y4gtzpaftor940ypobn1m0as",
   },
   {
@@ -45,7 +45,7 @@ export const Movies: Movie[] = [
     director: "Ari Aster",
     date: "2019",
     img: "/movies/midsommar.jpg",
-    description: `Brightly-lit and colorful scenes somehow give off a sense of dread at every turn. Florence Pugh does wonderfully here.`,
+    description: `Bright, colorful, and beautiful on the surface, yet somehow filled with dread at every turn. Florence Pugh is phenomenal.`,
     url: "https://tv.apple.com/us/movie/midsommar/umc.cmc.3btmmnmdi8cci3gb2qupxli8o",
   },
   {
@@ -53,7 +53,7 @@ export const Movies: Movie[] = [
     director: "Phil Lord, Christopher Miller",
     date: "2026",
     img: "/movies/project_hail_mary.jpg",
-    description: `If you love the book, you'll love the movie. Nearly perfect adaptation as science-fiction.`,
+    description: `If you loved the book, you'll love the movie. A remarkably faithful adaptation that brings the heart and excitement of the story to film.`,
     url: "https://tv.apple.com/us/movie/project-hail-mary/umc.cmc.7jxdlxvz304lj3iwhtrhbe8fv",
   },
 ];
