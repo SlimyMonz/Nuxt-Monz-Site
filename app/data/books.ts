ac3796bce91deabbd4c4c7f154c938d9e731e2b2
Slimy Monster <code@monz.us>
2026-08-27 14:29:12 -0400

    Updated text to be more interesting.

--- app/data/books.ts
+++ app/data/books.ts
@@ -13,7 +13,7 @@ export const Books: Book[] = [
     author: "Cixin Liu & Ken Liu",
     date: "2014",
     img: "/books/three_body_problem.jpg",
-    description: `A slow-starting science fiction story that really starts to take off once it gets going. Will need to be patient at first, but ends up being a wild ride.`,
+    description: `A slow burn that takes some patience to get into, but becomes an absolutely wild ride once it gets going. Strange, ambitious, and full of fascinating ideas.`,
     url: "https://books.apple.com/us/book/the-three-body-problem/id856893409",
   },
   {
@@ -21,7 +21,7 @@ export const Books: Book[] = [
     author: "Andy Weir",
     date: "2021",
     img: "/books/project_hail_mary.jpg",
-    description: "Purely fun sci-fi with a touch of suspense and a lot of heart. Engaging without being sophisticated.",
+    description: `Purely fun sci-fi with a great mix of suspense, humor, and heart. Not overly complicated, just an incredibly engaging and enjoyable read.`,
     url: "https://books.apple.com/us/book/project-hail-mary/id1526997052"
   },
 ];
