877d6fb4e67c2c0fc42bb42ce488f1a651a66c68
Slimy Monster <code@monz.us>
2026-09-03 18:29:03 -0400

    Created mediacard component for re-use.

---
+++ app/components/MediaCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <ULink :to="url" target="_blank" class="block">
+        <UCard
+            class="h-full transition hover:ring-4 hover:ring-secondary"
+            :ui="{ body: 'p-0 sm:p-0' }"
+        >
+            <template #header>
+                <div class="flex flex-col gap-0.5">
+                    <h2
+                        class="text-left text-lg font-bold text-highlighted truncate"
+                    >
+                        {{ title }}
+                    </h2>
+                    <div class="flex items-baseline justify-between gap-2">
+                        <span class="text-left truncate">
+                            {{ creator }}
+                        </span>
+                        <span class="text-right">
+                            {{ date }}
+                        </span>
+                    </div>
+                </div>
+            </template>
+            <img
+                :src="imagePath"
+                :alt="`${title} by ${creator}`"
+                class="w-full object-cover block"
+            />
+            <template #footer>
+                <p>
+                    {{ textBody }}
+                </p>
+            </template>
+        </UCard>
+    </ULink>
+</template>
+<script setup lang="ts">
+interface Props {
+    url: string;
+    title: string;
+    creator: string;
+    date: string;
+    imagePath: string;
+    textBody: string;
+}
+
+defineProps<Props>();
+</script>
