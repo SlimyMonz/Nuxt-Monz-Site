dfdb222930df3717140c3130ff3c423641f4be67
Slimy Monster <code@monz.us>
2026-09-04 15:13:00 -0400

    Fixed annoying colorMode bug.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <template>
     <UApp>
-        <OilBackground v-if="colorMode.value === 'dark'" />
+        <OilBackground v-if="colorMode.preference === 'dark'" />
         <!-- Header Content -->
         <UHeader toggle-side="left">
             <template #title>
@@ -39,7 +39,10 @@
         <!-- Page Body Content -->
         <UMain>
             <UContainer
-            :class="{ 'bg-black/10 backdrop-blur-3xl pb-8 rounded-b-3xl': colorMode.value === 'dark' }"
+                :class="{
+                    'bg-black/10 backdrop-blur-3xl pb-8 rounded-b-3xl':
+                        colorMode.preference === 'dark',
+                }"
             >
                 <NuxtPage />
             </UContainer>
